92c8ae7919afc776ebc902d147afa0142bdb9c49
Ares9505 <arielic1995@gmail.com>
2023-03-01 16:58:58 -0800

    Iniciate components and importing at App.js

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Testimonial from './components/testimonial/Testimonial'
+import Nav from './components/nav/Nav'
+import Services from './components/services/Services'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <>
+    <div>App</div>
+    <Nav/>
+    </>
+  )
+}
+
+export default App
